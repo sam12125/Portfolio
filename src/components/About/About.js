58e3935083c68bac801548b51de2324d2951db16
Saiyam vaid <99545380+sam12125@users.noreply.github.com>
2023-03-15 09:25:19 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
 import Footer from "../Footer";
 
 function About() {
@@ -35,16 +33,6 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-
-        <Techstack />
-
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-        <Toolstack />
       </Container>
     </Container>
       {/* <Footer/> */}
